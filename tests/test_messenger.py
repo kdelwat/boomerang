c67653e38c424c8a20e4f955b76c2f236cb66959
Cadel Watson <cadel@cadelwatson.com>
2016-12-24 12:04:26 +1100

    Refactor Messenger tests to enforce DRY.

--- tests/test_messenger.py
+++ tests/test_messenger.py
@@ -52,135 +52,92 @@ def test_invalid_register(bot):
     assert response.status == 403
 
 
-def test_message_received(bot):
-    '''Makes a POST request simulating a message to the bot, checking that the
-    bot responds with the appropriate status.'''
+def message_handled_ok(server, message_type, message_content):
+    '''Makes a POST request to the server, simulating the given message being
+    sent, and ensure the server responds with a 200 OK status.
+
+    Args:
+        server: A Sanic server to test against.
+        message_type: A string of the type of message, given as the Messenger
+                      API string for the type.
+        message_content: A dict representing the JSON message to be tested.
+
+    Returns:
+        True if a 200 OK response is received, otherwise False.
+
+    '''
+
     message = {'entry': [{'id': '233231370449158',
-                          'messaging': [{'message': {'mid': 'mid.1482375186449:88d829fb30',
-                                                     'seq': 426185,
-                                                     'text': 'ping'},
+                          'messaging': [{message_type: message_content,
                                          'recipient': {'id': '233231370449158'},
                                          'sender': {'id': '1297601746979209'},
                                          'timestamp': 1482375186449}],
                           'time': 1482375186497}],
                'object': 'page'}
 
-    message_json = json.dumps(message)
+    message_json_string = json.dumps(message)
 
-    request, response = sanic_endpoint_test(bot._server,
+    request, response = sanic_endpoint_test(server,
                                             method='post',
                                             uri='/webhook',
-                                            data=message_json)
-    assert response.status == 200
+                                            data=message_json_string)
+
+    return response.status == 200
+
+
+def test_message_received(bot):
+    '''Ensure the server responds with a 200 status to a message received event
+    sent by the API.'''
+    message = {'mid': 'mid.1482375186449:88d829fb30',
+               'seq': 426185,
+               'text': 'ping'}
+    assert message_handled_ok(bot._server, 'message', message)
 
 
 def test_message_delivered(bot):
-    '''Makes a POST request simulating a message delivered event to the bot,
-    checking that the bot responds with the appropriate status.'''
-    message = {'entry': [{'id': '233231370449158',
-                          'messaging': [{'delivery': {'mids': ['mid.1458668856218:ed81099e15d3f4f233',
-                                                               'mid.1458668856218:ed81099e15d3f4f234'],
-                                                      'watermark': 1111111111,
-                                                      'seq': 30},
-                                         'recipient': {'id': '233231370449158'},
-                                         'sender': {'id': '1297601746979209'},
-                                         'timestamp': 1482375186449}],
-                          'time': 1482375186497}],
-               'object': 'page'}
+    '''Ensure the server responds with a 200 status to a message delivered event
+    sent by the API.'''
+    message = {'mids': ['mid.1458668856218:ed81099e15d3f4f233',
+                        'mid.1458668856218:ed81099e15d3f4f234'],
+               'watermark': 1111111111,
+               'seq': 30}
 
-    message_json = json.dumps(message)
-
-    request, response = sanic_endpoint_test(bot._server,
-                                            method='post',
-                                            uri='/webhook',
-                                            data=message_json)
-    assert response.status == 200
+    assert message_handled_ok(bot._server, 'delivery', message)
 
 
 def test_message_read(bot):
-    '''Makes a POST request simulating a message read event to the bot,
-    checking that the bot responds with the appropriate status.'''
-    message = {'entry': [{'id': '233231370449158',
-                          'messaging': [{'read': {'watermark': 1111111111,
-                                                  'seq': 30},
-                                         'recipient': {'id': '233231370449158'},
-                                         'sender': {'id': '1297601746979209'},
-                                         'timestamp': 1482375186449}],
-                          'time': 1482375186497}],
-               'object': 'page'}
+    '''Ensure the server responds with a 200 status to a message read event
+    sent by the API.'''
+    message = {'watermark': 1111111111,
+               'seq': 30}
 
-    message_json = json.dumps(message)
-
-    request, response = sanic_endpoint_test(bot._server,
-                                            method='post',
-                                            uri='/webhook',
-                                            data=message_json)
-    assert response.status == 200
+    assert message_handled_ok(bot._server, 'read', message)
 
 
 def test_postback(bot):
-    '''Makes a POST request simulating a postback event to the bot,
-    checking that the bot responds with the appropriate status.'''
-    message = {'entry': [{'id': '233231370449158',
-                          'messaging': [{'postback': {'payload': 'dummy_payload'},
-                                         'recipient': {'id': '233231370449158'},
-                                         'sender': {'id': '1297601746979209'},
-                                         'timestamp': 1482375186449}],
-                          'time': 1482375186497}],
-               'object': 'page'}
+    '''Ensure the server responds with a 200 status to a postback event
+    sent by the API.'''
+    message = {'payload': 'dummy_payload'}
 
-    message_json = json.dumps(message)
-
-    request, response = sanic_endpoint_test(bot._server,
-                                            method='post',
-                                            uri='/webhook',
-                                            data=message_json)
-    assert response.status == 200
+    assert message_handled_ok(bot._server, 'postback', message)
 
 
 def test_postback_with_referral(bot):
-    '''Makes a POST request simulating a postback event to the bot, where a
-    referral is included, checking that the bot responds with the appropriate
-    status.
+    '''Ensure the server responds with a 200 status to a postback event
+    sent by the API, when the postback contains a referral.'''
+    message = {'payload': 'dummy_payload',
+               'referral': {'ref': 'dummy_referral_data',
+                            'source': 'SHORTLINK',
+                            'type': 'OPEN_THREAD'}}
 
-    '''
-    message = {'entry': [{'id': '233231370449158',
-                          'messaging': [{'postback': {'payload': 'dummy_payload',
-                                                      'referral': {'ref': 'dummy_referral_data',
-                                                                   'source': 'SHORTLINK',
-                                                                   'type': 'OPEN_THREAD'}},
-                                         'recipient': {'id': '233231370449158'},
-                                         'sender': {'id': '1297601746979209'},
-                                         'timestamp': 1482375186449}],
-                          'time': 1482375186497}],
-               'object': 'page'}
-
-    message_json = json.dumps(message)
-
-    request, response = sanic_endpoint_test(bot._server,
-                                            method='post',
-                                            uri='/webhook',
-                                            data=message_json)
-    assert response.status == 200
+    assert message_handled_ok(bot._server, 'postback', message)
 
 
 def test_referral(bot):
-    '''Makes a POST request simulating a referral event to the bot,
-    checking that the bot responds with the appropriate status.'''
-    message = {'entry': [{'id': '233231370449158',
-                          'messaging': [{'referral': {'ref': 'dummy_referral_data',
-                                                      'source': 'SHORTLINK',
-                                                      'type': 'OPEN_THREAD'},
-                                         'recipient': {'id': '233231370449158'},
-                                         'sender': {'id': '1297601746979209'},
-                                         'timestamp': 1482375186449}],
-                          'time': 1482375186497}],
-               'object': 'page'}
+    '''Ensure the server responds with a 200 status to a referral event
+    sent by the API.'''
+    message = {'ref': 'dummy_referral_data',
+               'source': 'SHORTLINK',
+               'type': 'OPEN_THREAD'}
 
-    message_json = json.dumps(message)
-
-    request, response = sanic_endpoint_test(bot._server,
-                                            method='post',
-                                            uri='/webhook',
-                                            data=message_json)
-    assert response.status == 200
+    assert message_handled_ok(bot._server, 'referral', message)
